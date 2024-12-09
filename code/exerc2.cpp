ff4d73345db3096349d5951756dd6ba1f62dab83
Rebar310 <rebar310@student.liu.se>
2024-12-09 16:20:07 +0100

    påbörjat exercise 2

--- code/exerc2.cpp
+++ code/exerc2.cpp
@@ -1,3 +1,20 @@
 /****************************
  * Lab 3: exercise 2        *
  ****************************/
+
+// I denna uppgift så ska programmet kunna läsa en fil med punkter (x,y) 
+// och sedan kunna räkna ut den totala distansen som m vi rest i den ordningen som punkterna ges. 
+// Alltså så ska den beräkna ut resultanten. Den totala resultanten kommer att bli längden av pythagoras sats 
+// som fås ut mellan varje punkt.
+
+#include <iostream>
+
+
+int main() {
+
+
+	return 0;
+
+
+
+}
